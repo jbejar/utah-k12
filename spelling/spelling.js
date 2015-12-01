451b9dc7395868cf4078d38cd0410c68a7606bf5
Wesley Young <wesley.young.portfolio@gmail.com>
2015-12-01 08:30:06 -0700

    test working

--- spelling/spelling.js
+++ spelling/spelling.js
@@ -2,13 +2,13 @@
 
 function WordList(source, delimeter) {
     this.delimeter = delimeter || " ";
-    this.words = source.split(this.delimeter);;
+    this.words = source.split(this.delimeter);
     this.size = function(){
         return this.words.length;
-    }
+    };
     this.get= function (i) {
         return new Word(this.words[i]);
-    }
+    };
     this.sort = function(){
         this.words.sort()
     }
@@ -18,13 +18,13 @@ function WordList(source, delimeter) {
 function Word(src, language) {
     this.src = src;
     this.voice = window.speechSynthesis.getVoices()[1];
-    this.language = "en" || language;
-    this.translation = this.language == 'en' ? new Word('','') : null;
+    this.language = language || "en";
+    this.translation = this.language == 'en' ? new Word('word','fr') : null;
     this.say = function() {
         var msg = new SpeechSynthesisUtterance(this.src);
         msg.voice = this.voice;
         window.speechSynthesis.speak(msg);
-    }
+    };
     this.spelledRight = function(word) {
         return word.toUpperCase() == src.toUpperCase();
     }
